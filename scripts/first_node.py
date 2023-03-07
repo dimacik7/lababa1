bc6cfc77d04548c4a631f13cf55249a8590a2661
dimacik7 <dimy6ik@mail.ru>
2023-03-07 13:27:22 +0300

    Вывод времени с интервалом 5 секун

--- scripts/first_node.py
+++ scripts/first_node.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+import datetime as dt
+from time import sleep
 
 def main():
-    print(f"Hello ROS World!")
-
+    while True:
+        now = dt.datetime.now()
+        times = now.strftime("%H:%M:%S")
+        print("Текущее время в ЧЧ:ММ:СС - ",times)
+        sleep(5)
 
 if __name__ == "__main__":
     main()
